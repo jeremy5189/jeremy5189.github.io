42a7dcf51b56b3d6b06c55cdf474e335fb956501
Jeremy Yen <jeremy5189@gmail.com>
2016-08-06 15:48:15 +0200

    Initial Commit

---
+++ assets/js/content.js
@@ -0,0 +1,286 @@
+var CONTENT = {
+    'en': {
+        sidebar: {
+            about_me      : 'About Me',
+            my_works      : 'My Works',
+            what_i_know   : 'What I Know',
+            my_experience : 'My Experience',
+            contact_me    : 'Contact Me',
+            switch_lang   : '切換語言',
+            target_lang   : 'zh-tw'
+        },
+        about_me: {
+            quote: 'Also known as Jing Bo Yen. Is a developer and traveller.',
+            learn_more : 'Learn more',
+            visit_blog : 'Visit my blog'
+        },
+        my_works_section: {
+            work_list: [
+                {
+                    img_src   : 'images/works/hahow-frontend.png',
+                    link_to   : 'https://hahow.in/cr/frontend-web-starter',
+                    title     : 'Frontend Web Development for Beginner (Online Video Course)',
+                    text      : 'Achived 350 student signup for this online video course within 30 days.',
+                    link_text : 'View course page',
+                    enabled   : true
+                },
+                {
+                    img_src   : 'images/works/hahow-backend.png',
+                    link_to   : 'https://hahow.in/cr/laravel-backend',
+                    title     : 'Laravel Backend Web Development for Beginner (Online Video Course)',
+                    text      : 'Achived --- student signup for this online video course within 30 days.',
+                    link_text : 'View course page',
+                    enabled   : true
+                },
+                {
+                    img_src   : 'images/works/ntust-lost.png',
+                    link_to   : 'http://lost.ntust.edu.tw',
+                    title     : 'NTUST Lost and Found System',
+                    text      : 'Development started around 2013, got online around 2014. Now severed as the official lost and found system in the NTUST and maintain by the Student Affairs Office',
+                    link_text : 'Visit system',
+                    enabled   : true
+                },
+                {
+                    img_src   : 'images/works/bi_charts.jpg',
+                    link_to   : 'http://billows.com.tw/',
+                    title     : 'Billows Security Portal / Log Master',
+                    text      : 'Company product, see website for more detail.',
+                    link_text : 'Product details',
+                    enabled   : true
+                },
+                {
+                    img_src   : 'images/works/ntust-patent.png',
+                    link_to   : '#',
+                    title     : 'NTUST Interactive Seat Selection System',
+                    text      : 'This is a temperary activity site for NTUST Collage of IP. Providing interactive conference room seats selection for attendees.',
+                    link_text : 'Site offline',
+                    enabled   : false
+                },
+            ]
+        },
+        what_i_know: {
+            what_i_know: 'What I Know',
+            text: 'I\'m a backend web developer. The following figure is a brief summary of my skills.',
+            skills: [
+                {
+                    icon  : 'code',
+                    title : 'PHP / Laravel',
+                    text  : 'Five years of native PHP experience, one and a half year of Laravel framework experience.'
+                },
+                {
+                    icon  : 'codepen',
+                    title : 'Node.js / Sails.js',
+                    text  : 'Two years of Node.js experience, both backend development and big data analysis.'
+                },
+                {
+                    icon  : 'code-fork',
+                    title : 'Git',
+                    text  : 'Four years of git version control experience. Collaborating with three commerical team up to ten developer.'
+                },
+                {
+                    icon  : 'terminal',
+                    title : 'Ubuntu Server / Amazon AWS',
+                    text  : 'Four years of Ubuntu Server and CentOS experience. Building and maintaining multi-purpose server on Amazon EC2.'
+                },
+                {
+                    icon  : 'desktop',
+                    title : 'Frontend',
+                    text  : 'HTML, CSS, Native JS, jQuery, Highcharts, Bootstrap, Vue.js'
+                },
+                {
+                    icon  : 'chain',
+                    title : 'Other',
+                    text  : 'Python, MySQL, ElasticSearch, Network administration, C/C++, Java, VB.Net, C#.Net, English (C1), Deutsch(A1.1)'
+                }
+            ]
+        },
+        my_experince_section: {
+            experiences: [
+                {
+                    title : 'Germany, Technical University of Cologne (TH Köln), Exchange Student',
+                    date  : '2016.3 - 2016.8',
+                    text  : 'Major: International Business',
+                    link  : 'https://www.th-koeln.de/',
+                    link_text : 'Visit TH Köln'
+                },
+                {
+                    title : 'Taipei, PyPAT IP Services, Big Data System Administrator',
+                    date  : '2015.9 - 2016.1',
+                    text  : '- Develop tools to extract and process big amount of patent data using Node.js and Python. <br/>- Build and administrate several ElasticSearch server on Amazon EC2.',
+                    link  : 'http://pypat.com/',
+                    link_text : 'Visit PyPAT'
+                },
+                {
+                    title : 'Taipei, Billows Technology Co., Ltd, Full Stack Developer',
+                    date  : '2015.5 - 2016.2',
+                    text  : '- Maintain existing products with natvie PHP and ElasticSearch. <br/>- Develop new product with Laravel. <br/>- Maintaining serveral ESXi VM host and client',
+                    link  : 'http://billows.com.tw/',
+                    link_text : 'Visit Billows'
+                },
+                {
+                    title : 'Taipei, Hacks in Taiwan Conference (HITCON) 2015, Interpreter',
+                    date  : '2015.8.26 - 29',
+                    text  : 'Provide simultaneous interpreting for both English and Chinese.',
+                    link  : 'https://hitcon.org/2015/',
+                    link_text : 'Visit HITCON'
+                },
+                {
+                    title : 'New York, National Model United Nations (NMUN) 2015, Head of NTUST delegation',
+                    date  : '2015.3.29 - 4.2',
+                    text  : 'As the head of NTUST delegation to NMUN 2015, I\'m responsible for both academic training and administration affairs.',
+                    link  : 'http://www.nmun.org/nmun_ny.html',
+                    link_text : 'Visit NMUN'
+                },
+                {
+                    title : 'Taipei, Students\' Information Technology Conference (STICON), Agenda Committee',
+                    date  : '2014 - 2016',
+                    text  : 'The founding purpose and goal of SITCON is to provide a platform for students who are interested in IT to speak and promote their work. Therefore, the Agenda Committee have done our best job to pick and select every outstanding paper which we had received.',
+                    link  : 'http://sitcon.org/',
+                    link_text : 'Visit STICON'
+                }
+            ]
+        }
+    },
+    'zh-tw': {
+        sidebar: {
+            about_me      : '關於我',
+            my_works      : '作品集',
+            what_i_know   : '專業技能',
+            my_experience : '經歷',
+            contact_me    : '聯絡我',
+            switch_lang   : 'English',
+            target_lang   : 'en'
+        },
+        about_me: {
+            quote: 'Also known as Jing Bo Yen. Is a developer and traveller.',
+            learn_more : '繼續閱讀',
+            visit_blog : '部落格'
+        },
+        my_works_section: {
+            work_list: [
+                {
+                    img_src   : 'images/works/hahow-frontend.png',
+                    link_to   : 'https://hahow.in/cr/frontend-web-starter',
+                    title     : 'Hahow 前端網頁設計入門課程講師',
+                    text      : '線上課程開課募資一個月內達標 1000%，目前已順利開課',
+                    link_text : '檢視課程頁面',
+                    enabled   : true
+                },
+                {
+                    img_src   : 'images/works/hahow-backend.png',
+                    link_to   : 'https://hahow.in/cr/laravel-backend',
+                    title     : 'Hahow Laravel 後端網頁設計入門課程講師',
+                    text      : '線上課程募資開始後 10 天即達標，目前備課中',
+                    link_text : '檢視課程頁面',
+                    enabled   : true
+                },
+                {
+                    img_src   : 'images/works/ntust-lost.png',
+                    link_to   : 'http://lost.ntust.edu.tw',
+                    title     : '臺灣科技大學失物招領系統',
+                    text      : '2013 年底開始開發，2014 年中正式上線，目前是臺科大官方遺失物招領系統，由學務處負責日常更新',
+                    link_text : '前往系統',
+                    enabled   : true
+                },
+                {
+                    img_src   : 'images/works/bi_charts.jpg',
+                    link_to   : 'http://billows.com.tw/',
+                    title     : '竣盟科技 Security Portal / Log Master',
+                    text      : '任職於竣盟科技時所負責的產品，細節請參考公司網站',
+                    link_text : '前往公司網站',
+                    enabled   : true
+                },
+                {
+                    img_src   : 'images/works/ntust-patent.png',
+                    link_to   : '#',
+                    title     : '臺灣科技大學專利所互動選位系統',
+                    text      : '由臺科大專利所委託製作之研討會報名選位系統，能快速處理研討會報名及加速對帳流程，並提供參加者互動式選擇會議廳座位之功能',
+                    link_text : 'Site offline',
+                    enabled   : false
+                },
+            ]
+        },
+        what_i_know: {
+            what_i_know: '專業技能',
+            text: '我的專長是網頁後端設計師，但也掌握一些前端技能，以下是我的技能樹',
+            skills: [
+                {
+                    icon  : 'code',
+                    title : 'PHP / Laravel',
+                    text  : '五年 Native PHP 開發經驗，一年半 Laravel 開發經驗'
+                },
+                {
+                    icon  : 'codepen',
+                    title : 'Node.js / Sails.js',
+                    text  : '兩年 Node.js 開發經驗，曾使用 Sails.js 開發網頁後端，也曾使用 Node.js 做大數據分析'
+                },
+                {
+                    icon  : 'code-fork',
+                    title : 'Git',
+                    text  : '四年 Git 版本控制經驗，曾與三個商業團隊、十個以上的開發者一同協作專案'
+                },
+                {
+                    icon  : 'terminal',
+                    title : 'Ubuntu Server / Amazon AWS',
+                    text  : '四年 Ubuntu Server 及 CentOS 管理經驗，包括使用 Amazon EC2 建置並管理伺服器'
+                },
+                {
+                    icon  : 'desktop',
+                    title : 'Frontend',
+                    text  : 'HTML, CSS, Native JS, jQuery, Highcharts, Bootstrap, Vue.js'
+                },
+                {
+                    icon  : 'chain',
+                    title : 'Other',
+                    text  : 'Python, MySQL, ElasticSearch, Network administration, C/C++, Java, VB.Net, C#.Net, English (C1), Deutsch(A1.1)'
+                }
+            ]
+        },
+        my_experince_section: {
+            experiences: [
+                {
+                    title : '德國科隆科技大學 交換學生',
+                    date  : '2016.3 - 2016.8',
+                    text  : '主修國際企業',
+                    link  : 'https://www.th-koeln.de/',
+                    link_text : '科隆科技大學網站'
+                },
+                {
+                    title : '台北桂桂科技(PyPAT)有限公司 大數據分析及系統管理',
+                    date  : '2015.9 - 2016.1',
+                    text  : '- Develop tools to extract and process big amount of patent data using Node.js and Python. <br/>- Build and administrate several ElasticSearch server on Amazon EC2.',
+                    link  : 'http://pypat.com/',
+                    link_text : 'PyPAT 公司介紹'
+                },
+                {
+                    title : 'Taipei, Billows Technology Co., Ltd, Full Stack Developer',
+                    date  : '2015.5 - 2016.2',
+                    text  : '- Maintain existing products with natvie PHP and ElasticSearch. <br/>- Develop new product with Laravel. <br/>- Maintaining serveral ESXi VM host and client',
+                    link  : 'http://billows.com.tw/',
+                    link_text : 'Visit Billows'
+                },
+                {
+                    title : 'Taipei, Hacks in Taiwan Conference (HITCON) 2015, Interpreter',
+                    date  : '2015.8.26 - 29',
+                    text  : 'Provide simultaneous interpreting for both English and Chinese.',
+                    link  : 'https://hitcon.org/2015/',
+                    link_text : 'Visit HITCON'
+                },
+                {
+                    title : 'New York, National Model United Nations (NMUN) 2015, Head of NTUST delegation',
+                    date  : '2015.3.29 - 4.2',
+                    text  : 'As the head of NTUST delegation to NMUN 2015, I\'m responsible for both academic training and administration affairs.',
+                    link  : 'http://www.nmun.org/nmun_ny.html',
+                    link_text : 'Visit NMUN'
+                },
+                {
+                    title : 'Taipei, Students\' Information Technology Conference (STICON), Agenda Committee',
+                    date  : '2014 - 2016',
+                    text  : 'The founding purpose and goal of SITCON is to provide a platform for students who are interested in IT to speak and promote their work. Therefore, the Agenda Committee have done our best job to pick and select every outstanding paper which we had received.',
+                    link  : 'http://sitcon.org/',
+                    link_text : 'Visit STICON'
+                }
+            ]
+        }
+    }
+};
